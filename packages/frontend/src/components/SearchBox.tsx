743cf71ff40d04c60607cca1be45c20d97081352
Marko Gonchar <marko@pepega.club>
2022-05-30 16:32:57 +0200

    frontend: search box: make suggestions clickable, add clear button

--- packages/frontend/src/components/SearchBox.tsx
+++ packages/frontend/src/components/SearchBox.tsx
@@ -2,41 +2,26 @@ export default function SearchBox() {
 	return <>
 		<div class="search search--show-suggestions">
 			<form class="search__form">
-				<div class="search__form__input">
-					<input class="search__form__input__text" type="search" aria-label="Search query" name="q" />
-					<button class="search__form__input__reset" type="button" aria-label="Reset search query">
+				<div class="search__form__input" tabindex="-1">
+					<input class="search__form__input__text" type="search" placeholder="Search query…" aria-label="Search query" name="q" />
+					<button class="search__form__input__clear" type="reset" aria-label="Reset search query">
 						<MdiClose />
 					</button>
 					<button class="search__form__input__submit" type="submit" aria-label="Submit search">
 						<MdiSearch />
 					</button>
 				</div>
-				<ul class="search__form__suggestions" aria-label="Search suggestions">
-					<li>pepega</li>
-					<li>pepegag emote</li>
-					<li>pepegapog emote</li>
-					<li>pepegaaim</li>
-					<li>pepega mod</li>
-					<li>pepega</li>
-					<li>pepegag emote</li>
-					<li>pepegapog emote</li>
-					<li>pepegaaim</li>
-					<li>pepega mod</li>
-					<li>pepega</li>
-					<li>pepegag emote</li>
-					<li>pepegapog emote</li>
-					<li>pepegaaim</li>
-					<li>pepega mod</li>
-					<li>pepega</li>
-					<li>pepegag emote</li>
-					<li>pepegapog emote</li>
-					<li>pepegaaim</li>
-					<li>pepega mod</li>
-					<li>pepega</li>
-					<li>pepegag emote</li>
-					<li>pepegapog emote</li>
-					<li>pepegaaim</li>
-					<li>pepega mod</li>
+				<ul class="search__form__suggestions" aria-label="Search suggestions" tabindex="-1">
+					<li><button type="button">pepega</button></li>
+					<li><button type="button">pepegag emote</button></li>
+					<li><button type="button">pepegapog emote</button></li>
+					<li><button type="button">pepegaaim</button></li>
+					<li><button type="button">pepega mod</button></li>
+					<li><button type="button">pepega</button></li>
+					<li><button type="button">pepegag emote</button></li>
+					<li><button type="button">pepegapog emote</button></li>
+					<li><button type="button">pepegaaim</button></li>
+					<li><button type="button">pepega mod</button></li>
 				</ul>
 			</form>
 		</div>
